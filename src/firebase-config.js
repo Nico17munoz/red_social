4a49446089d35ecd8aa1cb6fc9355d38742832d0
Nico17munoz <nico.munoz11@gmail.com>
2021-03-15 14:51:32 -0300

    inicios

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyDos0vFI7icTGg0PSd9_3SP7LXda0yPyAo",
+    authDomain: "redsocial-react-firebase.firebaseapp.com",
+    projectId: "redsocial-react-firebase",
+    storageBucket: "redsocial-react-firebase.appspot.com",
+    messagingSenderId: "123808812791",
+    appId: "1:123808812791:web:1f4a7323293c24c2d58df2",
+    measurementId: "G-PKXMTNVLSW"
+  };
